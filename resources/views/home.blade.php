3c023f5a18fadb48e477373cebe1c2589b592975
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-11-22 09:48:49 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="w-full max-w-4xl text-white">
                 <h2 class="sm:text-2xl text-lg font-light mb-2">Welcome to VillaVeh</h2>
                 <h1 class="sm:text-4xl text-2xl font-bold mb-6">Book an appartment for your vacation</h1>
-                <div  x-data="{ open: false }" class="lg:flex gap-3 p-4">
+                <div  x-data="{ open: false }" class="lg:flex gap-3 justify-between">
                     <a href="#" class="hidden sm:flex btn-primary">Learn more</a>
                     <button type="submit" class="lg:hidden block btn-primary">
                         Book Now
